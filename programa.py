088915208856290d182784a5ed4f217c48b14317
Eduardo <eduardo.e@aluno.ifsp.edu.br>
2022-08-24 19:30:39 -0300

    turned number conversion logic into two functions

--- programa.py
+++ programa.py
@@ -1,11 +1,6 @@
-while True:
-    print("1) Converter da base 10 para outra base")
-    print("2) Converter de outra base para a base 10")
-    print("999) Sair")
-    novoNumero = []
-    numeroConvertido = ""
-    escolha = int(input())
-    if escolha == 1:
+def converterBase10ParaOutraBase(numero):
+        novoNumero = []
+        numeroConvertido = ""
         numero = int(input("Numero na base 10 a ser convertido: "))
         numeroOriginal = numero
         base = int(input("Nova base: "))
@@ -16,17 +11,20 @@ while True:
             novoNumero.append(resto)
             numero = int(numero/ base)
 
-            
-            
         novoNumero.reverse()
         for numero in novoNumero:
             numeroConvertido+=str(numero)
 
-        
-        print(numeroOriginal, end='\N{SUBSCRIPT ONE}\N{SUBSCRIPT ZERO}')
-        
-        print(f" = {numeroConvertido} na base {base}")
-    elif escolha == 2:
+        resultados = {
+            "numeroOriginal": numeroOriginal, 
+            "numeroConvertido": numeroConvertido, 
+            "base": base
+        }
+
+        return resultados
+
+def converterOutraBaseParaBase10(numero):
+        novoNumero = []
         numeroConvertido = 0
         numero = int(input("Numero a ser convertido: "))
         base = int(input("Base do numero: "))
@@ -38,6 +36,29 @@ while True:
             resultado = pow(base, i) * int(novoNumero[len(novoNumero) - i - 1])
             numeroConvertido += resultado
 
-        print(numeroConvertido)
+
+        resultados = {
+            "numeroOriginal": numero, 
+            "numeroConvertido": numeroConvertido, 
+            "base": base
+        }
+
+        return resultados
+
+while True:
+    print("1) Converter da base 10 para outra base")
+    print("2) Converter de outra base para a base 10")
+    print("999) Sair")
+    escolha = int(input())
+    if escolha == 1:
+        dicionarioResultados = converterBase10ParaOutraBase(escolha)
+        print(dicionarioResultados.get("numeroOriginal"), end='\N{SUBSCRIPT ONE}\N{SUBSCRIPT ZERO}')
+        print(" = "+ str(dicionarioResultados.get("numeroConvertido")) +  " na base "+str(dicionarioResultados.get("base")))
+    elif escolha == 2:
+        dicionarioResultados = converterOutraBaseParaBase10(escolha)
+        print(str(dicionarioResultados.get("numeroOriginal"))+" na base " + str(dicionarioResultados.get("base")) + " = ", end='')
+        print(str(dicionarioResultados.get("numeroConvertido")), end='\N{SUBSCRIPT ONE}\N{SUBSCRIPT ZERO}\n')
     elif escolha == 999:
         break
+
+
